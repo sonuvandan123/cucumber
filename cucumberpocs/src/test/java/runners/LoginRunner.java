2187aa89e7d3857537e2963d9548881709549a80
pc <pc@DESKTOP-TGCDU3N>
2024-10-09 23:32:47 +0530

    hello

--- cucumberpocs/src/test/java/runners/LoginRunner.java
+++ cucumberpocs/src/test/java/runners/LoginRunner.java
@@ -4,7 +4,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/featurefiles", glue = { "steps" }, plugin = {
+@CucumberOptions(features = "src/test/resources/featurefiles/login.feature", glue = { "steps" }, plugin = {
 		"pretty", "html:target/cucumber-reports/cucumber-html-report.html",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }
 
--- cucumberpocs/src/test/resources/featurefiles/addToCart.feature
+++ cucumberpocs/src/test/resources/featurefiles/addToCart.feature
@@ -6,4 +6,4 @@ Scenario: Verify the Same Product is Added to the Cart
 Given the user is on the inventory page
 When the user clicks on "Add to Cart" for the product "Sauce Labs Bike Light"
 And the user clicks on the cart icon
-Then the user should see that the product "Sauce Labs Bike Light" is in the cart
+Then the user should see that the product "Sauce Labs Bike Lights" is in the cart
